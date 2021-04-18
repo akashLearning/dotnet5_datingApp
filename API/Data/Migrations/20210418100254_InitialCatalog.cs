b2151597b56d5d1954288837eb75df25b9aa0c46
Akash Patil <patil.akash629@gmail.com>
2021-04-18 17:07:12 +0530

    [DotNet5] Basic get API complete for Users with EntityCore framework

---
+++ API/Data/Migrations/20210418100254_InitialCatalog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class InitialCatalog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.AppUserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int AppUserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
